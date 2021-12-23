c4321071d972af7082354416e202b9933bef02a8
nikhil yadav <iamnikhilyadav27@gmail.com>
2021-12-23 19:08:57 +0530

    product schema added

---
+++ src/database/product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  desc: { type: String, required: true },
+  img: { type: String, required: true },
+  categories: { type: Array },
+  size: { type: String },
+  color: { type: String },
+  price: { type: Number, required: true },
+});
+
+module.exports = productSchema;
