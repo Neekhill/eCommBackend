1e82cfdc11e4b12131cf8a66209c0292e3fb02d7
nikhil yadav <iamnikhilyadav27@gmail.com>
2021-12-23 19:00:06 +0530

    user schema added

---
+++ src/database/schema/user.js
@@ -0,0 +1,17 @@
+const mogoose = require("mongoose");
+const Schema = mogoose.Schema;
+
+const userSchema = new Schema(
+  {
+    usermame: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true } // this will created at, updated at timestamps
+);
+
+module.exports = userSchema;
