056910ef298c4e6eea928f0511419d682342d72b
nikhil yadav <iamnikhilyadav27@gmail.com>
2021-12-23 19:18:31 +0530

    cart schema added

---
+++ src/database/schema/cart.js
@@ -0,0 +1,22 @@
+const mogoose = require("mongoose");
+const Schema = mogoose.Schema;
+
+const cartSchema = new Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: { type: String },
+      },
+      {
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true } // this will created at, updated at timestamps
+);
+
+module.exports = cartSchema;
